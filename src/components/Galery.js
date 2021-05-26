2f419826e39b70d91422c294c5888f2eebe5d1f4
Souriya Phongsavanh <benjamin.franklin59777@gmail.com>
2021-05-26 15:46:08 +0200

    souriya

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-const Galery = () => {
-    const [myDate, setDate] = useState('')
-    useEffect(() => {
-        console.log('useEffect')
-        fetch('https://jsonplaceholder.typicode.com/photos').then(
-            () => {
-                
-            }
-        )
-    }, [])
+function Galery() {
+  const [myDate, setDate] = useState("");
+  const [imgList, setImgList] = useState([]);
 
-    setInterval(() => {
-        setDate(new Date().toString())
-    }, 1000)
-    return (
-        <div>
-            Hello!
-        </div>
-    )
-}
+  // COMPONENT DID MOUNT
+  useEffect(() => {
+    console.log("useEffect Galery");
 
-export default Galery
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setImgList(json.slice(0, 9))
+      });
+  }, []);
+
+  useEffect(() => {
+    if(imgList.length < 1) {
+      return;
+    }
+    
+    console.log(imgList);
+  }, [imgList])
+
+  setInterval(() => setDate(new Date().toString()), 1000);
+
+  return <div>Hello!</div>;
+};
+
+export default Galery;
