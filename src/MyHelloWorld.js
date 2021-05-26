d1a72d2caefc25f736293bd3c1699e6fbd508429
pcadoret <47423688+pcadoret@users.noreply.github.com>
2021-05-26 10:38:54 +0200

    MyHelloWorld component

---
+++ src/MyHelloWorld.js
@@ -0,0 +1,11 @@
+import "./helloWorld.scss"
+
+const MyHelloWorld = (props) => {
+    return (
+        <div className="helloWorld">
+            Hello WORLD!
+        </div>
+    )
+}
+
+export default MyHelloWorld;
